e51a8c9cf1250a9f3613fc9ff7c4b59500342160
Michael Rascati <MRascati@Michael-iMac27-EL.local>
2017-03-17 15:39:59 -0400

    tagged

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='filesentry',
+    packages=['filesentry']
     version='0.1.0',
     description='Tool to compare two directories and their contents',
-    url='https://pypi.python.org/pypi/FileSentry',
     author='Michael Rascati',
-    packages=find_packages(),
-    install_requires=['filecmp','dircmp','os','sys','colorama'],
-    entry_points={
-        'console_scripts': [
-            'filesentry = filesentry:print_diff_files'
-        ]
-    }
-    zip_safe=False
+    url='https://github.com/rascati/filesentry',
+    download_url = 'https://github.com/rascati/filesentry/archive/0.1.0.tar.gz', # I'll explain this in a second
+
+
+    # packages=find_packages(),
+    # install_requires=['filecmp','dircmp','os','sys','colorama'],
+    # # entry_points={
+    # #     'console_scripts': [
+    # #         'filesentry = filesentry:print_diff_files'
+    # #     ]
+    # # }
+    # zip_safe=False
 )
